f38960998ed8653a9fc52e862eb9bfc6ef9a6762
Philip Bruland <philip.bruland@gmail.com>
2023-10-12 20:38:49 +0100

    Basic Jest and Typescript setup

---
+++ src/checkSetup/sum.test.ts
@@ -0,0 +1,5 @@
+import { summer } from './sum';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(summer(1, 2)).toBe(3);
+});
---
+++ src/checkSetup/sum.ts
@@ -0,0 +1,5 @@
+function summer(a: number, b: number): number {
+  return a + b;
+}
+
+export { summer };
