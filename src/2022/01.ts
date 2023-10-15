eeb1a023dbdca0f5bb01fe395b96f96a47aad4a1
Philip Bruland <philip.bruland@gmail.com>
2023-10-15 11:34:46 +0100

    Refactor Q1 to make it more readable and concise

--- src/2022/01.ts
+++ src/2022/01.ts
@@ -1,20 +1,14 @@
-function getCaloriesFromMostElf(calorieLedger: string): number {
-  const ledgerPerElf: Array<string> = calorieLedger.split('\n\n');
-  const parsedLedgerPerElf: Array<Array<number>> = ledgerPerElf.map((ledger) => {
-    const individualCalorieValueStrings: Array<string> = ledger.split('\n');
-    const individualCalorieValues: Array<number> = individualCalorieValueStrings.map((value) => parseInt(value));
-    return individualCalorieValues;
-  });
-  const elfValueArray: Array<number> = parsedLedgerPerElf.map((ledger) => {
-    const calorieTotalForElf: number = ledger.reduce((previousValue: number, currentValue: number) => previousValue + currentValue);
-    return calorieTotalForElf;
-  });
-  
-  const sortedElfValueArray = elfValueArray.sort((a: number, b:number) => b - a);
+const parseCalorieLedger = (calorieLedger: string): Array<Array<number>> =>
+  calorieLedger.split('\n\n').map((ledger): Array<number> =>
+    ledger.split('\n').map((value): number => parseInt(value, 10)));
 
-  const highestCalories = sortedElfValueArray[0];
+const getTotalCaloriesForSingleElf =
+  (elfLedger: Array<number>): number =>
+    elfLedger.reduce((previousValue: number, currentValue: number) => previousValue + currentValue);
 
-  console.log(sortedElfValueArray);
-  return highestCalories;
-}
+const getCaloriesFromMostElf = (calorieLedger: string): number =>
+  parseCalorieLedger(calorieLedger)
+    .map(getTotalCaloriesForSingleElf)
+    .sort((a: number, b:number) => b - a)[0];
+
 export default getCaloriesFromMostElf;
