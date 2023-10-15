cd1b1fe892d6b2d49ff66ba76b92d340992f0b39
Philip Bruland <philip.bruland@gmail.com>
2023-10-15 11:59:03 +0100

    Set up way to run with inputs

---
+++ src/2022/index.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-console */
+import { puzzleOneImput } from './puzzleInputs';
+import getCaloriesFromMostElf from './01';
+
+console.log('Question 1 - Part 1');
+console.log(getCaloriesFromMostElf(puzzleOneImput));
