90df4cce00842b3a8cee7913db4a6db3231f61af
Philip Bruland <philip.bruland@ovoenergy.com>
2023-12-27 16:17:22 +0000

    WIP day 1 part 2

--- src/2023/01.test.ts
+++ src/2023/01.test.ts
@@ -1,14 +1,33 @@
-import { getCalibrationValue, combineFirstAndLastNumberInLine } from './01';
+import {
+  getCalibrationValue,
+  combineFirstAndLastNumberInLine,
+} from './01';
 
 const testInput = `1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet`;
 
+const testInputWithWordNumbers = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`;
+
 test('can get the first and last number in the line', () => {
   expect(combineFirstAndLastNumberInLine('a1b2c3d4e5f')).toBe(15);
 });
 
+test('can get the first and last number in the line with wordnumbers', () => {
+  expect(combineFirstAndLastNumberInLine('abcone2threexyz')).toBe(13);
+});
+
 test('can get the calibration value from the document', () => {
   expect(getCalibrationValue(testInput)).toBe(142);
+});
+
+test('can get a calibration value from a document with word numbers', () => {
+  expect(getCalibrationValue(testInputWithWordNumbers)).toBe(281);
 });
--- src/2023/01.ts
+++ src/2023/01.ts
@@ -1,11 +1,58 @@
+const NUMBER_OF_LETTERS_TO_REPLACE = 1;
+const LETTER_REPLACE_OFFSET = 1;
+
+const replaceSecondChar = (
+  inputString: string,
+  matchString: string,
+  replacement: string,
+): string => {
+  let workingString = inputString;
+
+  while (workingString.indexOf(matchString) !== -1) {
+    const index = workingString.indexOf(matchString);
+    workingString = workingString.substring(0, index + LETTER_REPLACE_OFFSET)
+    + replacement
+    + workingString.substring(index + LETTER_REPLACE_OFFSET + NUMBER_OF_LETTERS_TO_REPLACE);
+  }
+
+  return workingString;
+};
+
+const replaceAllWordNumbers = (inputString: string): string => {
+  let workingString = inputString;
+  workingString = replaceSecondChar(workingString, 'one', '1');
+  workingString = replaceSecondChar(workingString, 'two', '2');
+  workingString = replaceSecondChar(workingString, 'three', '3');
+  workingString = replaceSecondChar(workingString, 'four', '4');
+  workingString = replaceSecondChar(workingString, 'five', '5');
+  workingString = replaceSecondChar(workingString, 'six', '6');
+  workingString = replaceSecondChar(workingString, 'seven', '7');
+  workingString = replaceSecondChar(workingString, 'eight', '8');
+  workingString = replaceSecondChar(workingString, 'nine', '9');
+
+  return workingString;
+};
+
 const combineFirstAndLastNumberInLine = (line: String) => {
-  const arrayOfNumbers = line.split('')
+  const arrayOfNumbers = line
+    .replace('one', '1')
+    .replace('two', '2')
+    .replace('three', '3')
+    .replace('four', '4')
+    .replace('five', '5')
+    .replace('six', '6')
+    .replace('seven', '7')
+    .replace('eight', '8')
+    .replace('nine', '9')
+    .split('')
     .filter((value: String) => !Number.isNaN(parseInt(value as string, 10)));
 
   return parseInt(`${arrayOfNumbers.at(0)}${arrayOfNumbers.at(-1)}`, 10);
 };
 
-const getCalibrationValue = (calibrationSheet: String) => calibrationSheet.split('\n')
+const getCalibrationValue = (calibrationSheet: String) => calibrationSheet
+  .split('\n')
+  .map(replaceAllWordNumbers)
   .map(combineFirstAndLastNumberInLine)
   .reduce((a: number, b: number) => a + b);
 
