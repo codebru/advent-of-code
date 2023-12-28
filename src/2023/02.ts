72d76acaacc5b7399623573518df3cacb3d865f8
Philip Bruland <philip.bruland@ovoenergy.com>
2023-12-28 16:56:36 +0000

    Day 2 Part1: Complete

--- src/2023/02.ts
+++ src/2023/02.ts
@@ -1,4 +1,4 @@
-type ExampleRound = {
+type Round = {
   gameId: number,
   red: number,
   green: number,
@@ -6,6 +6,9 @@ type ExampleRound = {
 };
 
 const ID_POSITION_IN_LINE = 1;
+const CUBES_IN_ROUND_RED = 12;
+const CUBES_IN_ROUND_GREEN = 13;
+const CUBES_IN_ROUND_BLUE = 14;
 
 const getId = (line: string): number => parseInt(line.split(' ')[ID_POSITION_IN_LINE], 10);
 
@@ -30,15 +33,25 @@ const getMaxOfCubeNumbers = (
 ): number => getCubeNumbers(line, cubeName)
   .reduce((previous, current) => (current > previous ? current : previous));
 
-const parseMaxForEachCube = (input: string) : ExampleRound[] => input
+const parseMaxForEachCube = (input: string) : Round[] => input
   .split('\n')
   .filter((line) => line.startsWith('Game'))
   .map((line) => ({
     gameId: getId(line), red: getMaxOfCubeNumbers(line, 'red'), green: getMaxOfCubeNumbers(line, 'green'), blue: getMaxOfCubeNumbers(line, 'blue'),
   }));
 
-const sumPossibleGamesOfCubes = (input: string) => 1;
+const possibleRound = (round: Round): boolean => round.red <= CUBES_IN_ROUND_RED
+&& round.green <= CUBES_IN_ROUND_GREEN
+&& round.blue <= CUBES_IN_ROUND_BLUE;
+
+const returnIdIfPossibleRound = (round: Round): number | null =>
+  (possibleRound(round) ? round.gameId : null);
+
+const sumPossibleGamesOfCubes = (input: string) => parseMaxForEachCube(input)
+  .map(returnIdIfPossibleRound)
+  .filter((gameId) => gameId !== null)
+  .reduce((previous, current) => (previous as number) + (current as number));
 
 export {
   getCubeNumbers, getId, parseMaxForEachCube, sumPossibleGamesOfCubes,
-}; 
+};
--- src/2023/index.ts
+++ src/2023/index.ts
@@ -1,10 +1,14 @@
 /* eslint-disable no-console */
-import puzzleOneImput from './puzzleImputs';
+import { puzzleOneImput, puzzleTwoInput } from './puzzleImputs';
 
 import { getCalibrationValue } from './01';
+import { sumPossibleGamesOfCubes } from './02';
 
 console.log('Question 1 - Part 1');
 console.log(getCalibrationValue(puzzleOneImput));
 
 console.log('Question 1 - Part 2');
 console.log(getCalibrationValue(puzzleOneImput, true));
+
+console.log('Question 2 - Part 1');
+console.log(sumPossibleGamesOfCubes(puzzleTwoInput));
