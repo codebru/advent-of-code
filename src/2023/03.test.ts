1755850f434fefe53f576317dee770710b8b626b
Philip Bruland <philip.bruland@gmail.com>
2024-01-13 23:13:44 +0000

    WIP 2023 Day 3 Part 1:  Locations and symbol adjacents

---
+++ src/2023/03.test.ts
@@ -0,0 +1,76 @@
+import {
+  addAllPartNumbers,
+  getPartLocationsAndLengths,
+  isSymbolAdjacent,
+} from './03';
+
+const TEST_INPUT = `467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`;
+
+const OVERALL_ANSWER = 4361;
+
+test('GIVE a valid game input THEN get the locations and legths of the numbers', () => {
+  expect(getPartLocationsAndLengths(`..223..
+.3...22
+2...1..`)).toEqual([
+    { location: { row: 0, collumn: 2 }, length: 3 },
+    { location: { row: 1, collumn: 1 }, length: 1 },
+    { location: { row: 1, collumn: 5 }, length: 2 },
+    { location: { row: 2, collumn: 0 }, length: 1 },
+    { location: { row: 2, collumn: 4 }, length: 1 },
+  ]);
+});
+
+test('GIVEN a location of a number WHEN it is symbol adjeacent THEN return true', () => {
+  expect(
+    isSymbolAdjacent(
+      TEST_INPUT,
+      {
+        location: { row: 6, collumn: 2 },
+        length: 3,
+      },
+    ),
+  ).toBe(true);
+  expect(
+    isSymbolAdjacent(
+      TEST_INPUT,
+      {
+        location: { row: 4, collumn: 0 },
+        length: 3,
+      },
+    ),
+  ).toBe(true);
+});
+
+test('GIVEN a location of a number WHEN it is not symbol adjeacent THEN return false', () => {
+  expect(
+    isSymbolAdjacent(
+      TEST_INPUT,
+      {
+        location: { row: 0, collumn: 6 },
+        length: 3,
+      },
+    ),
+  ).toBe(false);
+  expect(
+    isSymbolAdjacent(
+      TEST_INPUT,
+      {
+        location: { row: 5, collumn: 7 },
+        length: 2,
+      },
+    ),
+  ).toBe(false);
+});
+
+test('GIVEN a valid game input THEN get the answer', () => {
+  expect(addAllPartNumbers(TEST_INPUT)).toBe(OVERALL_ANSWER);
+});
---
+++ src/2023/03.ts
@@ -0,0 +1,91 @@
+const addAllPartNumbers = (engineSchematic: string): number => 1;
+
+const isNumber = (char: string): boolean => !Number.isNaN(Number(char));
+
+type Location = { row: number, collumn: number };
+type Length = number;
+type Part = { location: Location, length: Length };
+
+const parseEngineSchematic = (engineSchematic: string): string[][] => engineSchematic.split('\n')
+  .map((row) => row.split(''));
+
+const isSymbol = (char: string): boolean => char === '*' || char === '#' || char === '$' || char === '+';
+
+const isSymbolAdjacent = (engineSchematic: string, part: Part): boolean => {
+  const parsedEngineSchematic = parseEngineSchematic(engineSchematic);
+
+  // Row above
+  if (part.location.row - 1 >= 0) {
+    for (
+      let i = part.location.collumn - 1;
+      i < part.location.collumn + part.length + 1;
+      i += 1
+    ) {
+      if (i >= 0 && i < parsedEngineSchematic[part.location.row].length) {
+        if (isSymbol(parsedEngineSchematic[part.location.row - 1][i])) {
+          return true;
+        }
+      }
+    }
+  }
+
+  // That row
+  if (
+    part.location.collumn - 1 >= 0
+    && isSymbol(parsedEngineSchematic[part.location.row][part.location.collumn - 1])
+  ) return true;
+  if (
+    part.location.collumn + part.length < parsedEngineSchematic[part.location.row].length
+    && isSymbol(parsedEngineSchematic[part.location.row][part.location.collumn + part.length])
+  ) return true;
+
+  // Row below
+  if (part.location.row + 1 < parsedEngineSchematic.length) {
+    for (
+      let i = part.location.collumn - 1;
+      i < part.location.collumn + part.length + 1;
+      i += 1
+    ) {
+      if (i >= 0 && i < parsedEngineSchematic[part.location.row].length) {
+        if (isSymbol(parsedEngineSchematic[part.location.row + 1][i])) {
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
+};
+
+const getPartLocationsAndLengths = (engineSchematic: string): Part[] => {
+  const parts: Part[] = [];
+
+  parseEngineSchematic(engineSchematic)
+    .forEach((row, rowIndex) => {
+      row.forEach((collumn, collumnIndex) => {
+        if (isNumber(collumn)) {
+          if (collumnIndex === 0 || !isNumber(row[collumnIndex - 1])) {
+            let numberLength = collumnIndex;
+            while (numberLength <= row.length - 1 && isNumber(row[numberLength])) {
+              numberLength += 1;
+            }
+            numberLength -= collumnIndex;
+            parts.push({
+              location: {
+                row: rowIndex,
+                collumn: collumnIndex,
+              },
+              length: numberLength,
+            });
+          }
+        }
+      });
+    });
+  return parts;
+};
+
+export {
+  addAllPartNumbers,
+  getPartLocationsAndLengths,
+  isSymbolAdjacent,
+};
